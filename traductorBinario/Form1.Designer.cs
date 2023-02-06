9d121c2400ea5a90a795d8760081ff550b14462f
PieroPastor <102126264+PieroPastor@users.noreply.github.com>
2023-02-05 21:00:08 -0500

    archsUpdate

---
+++ traductorBinario/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace traductorBinario
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.findArch = new System.Windows.Forms.Button();
+            this.traducirBinario = new System.Windows.Forms.Button();
+            this.traducirLetras = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(55, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(699, 330);
+            this.textBox1.TabIndex = 0;
+            // 
+            // findArch
+            // 
+            this.findArch.AutoSize = true;
+            this.findArch.Location = new System.Drawing.Point(321, 361);
+            this.findArch.Name = "findArch";
+            this.findArch.Size = new System.Drawing.Size(177, 64);
+            this.findArch.TabIndex = 1;
+            this.findArch.Text = "Archivo";
+            this.findArch.UseVisualStyleBackColor = true;
+            this.findArch.Click += new System.EventHandler(this.findArch_Click);
+            // 
+            // traducirBinario
+            // 
+            this.traducirBinario.AutoSize = true;
+            this.traducirBinario.Location = new System.Drawing.Point(55, 361);
+            this.traducirBinario.Name = "traducirBinario";
+            this.traducirBinario.Size = new System.Drawing.Size(177, 64);
+            this.traducirBinario.TabIndex = 2;
+            this.traducirBinario.Text = "Traducir a Binario";
+            this.traducirBinario.UseVisualStyleBackColor = true;
+            this.traducirBinario.Click += new System.EventHandler(this.traducirBinario_Click);
+            // 
+            // traducirLetras
+            // 
+            this.traducirLetras.AutoSize = true;
+            this.traducirLetras.Location = new System.Drawing.Point(577, 361);
+            this.traducirLetras.Name = "traducirLetras";
+            this.traducirLetras.Size = new System.Drawing.Size(177, 64);
+            this.traducirLetras.TabIndex = 3;
+            this.traducirLetras.Text = "Traducir a Caracteres";
+            this.traducirLetras.UseVisualStyleBackColor = true;
+            this.traducirLetras.Click += new System.EventHandler(this.traducirLetras_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.traducirLetras);
+            this.Controls.Add(this.traducirBinario);
+            this.Controls.Add(this.findArch);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(822, 506);
+            this.Name = "Form1";
+            this.Text = "Traductor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button findArch;
+        private System.Windows.Forms.Button traducirBinario;
+        private System.Windows.Forms.Button traducirLetras;
+    }
+}
+
---
+++ traductorBinario/Form1.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace traductorBinario
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //BUSCA EL ARCHIVO QUE SE LEERÁ PARA COLOCAR EN LA CAJA DE TEXTO
+        private void findArch_Click(object sender, EventArgs e)
+        {
+            using (var arch = new OpenFileDialog())
+            {
+                if (arch.ShowDialog() == System.Windows.Forms.DialogResult.OK && !string.IsNullOrWhiteSpace(arch.SafeFileName))
+                {
+                    MessageBox.Show(arch.FileName, "Se abrió correctamente el archivo:");
+                    string archivo = arch.FileName;
+                    leerArchivo(archivo);
+                }
+                else
+                {
+                    MessageBox.Show("No se abrió ningún archivo.", "ERROR");
+                }
+            }
+        }
+
+        //ABRE EL ARCHIVO
+        private void leerArchivo(string ubicacionArchivo)
+        {
+            System.IO.StreamReader archivo = new System.IO.StreamReader(ubicacionArchivo);
+            textBox1.Text = copiarTexto(archivo);
+        }
+
+        //LEE EL ARCHIVO Y LO COLOCA EN LA CAJA DE TEXTO
+        private string copiarTexto(System.IO.StreamReader archivo)
+        {
+            string texto = null;
+            string linea;
+            int centinela = 1;
+
+            while((linea = archivo.ReadLine()) != null)
+            {
+                if(centinela == 1)
+                {
+                    texto += linea;
+                    centinela = 0;
+                }
+                else
+                {
+                    texto += (" " + linea);
+                }
+            }
+
+            return texto;
+        }
+
+        //INICIA LA TRADUCCIÓN DE BINARIO A CARACTERES
+        private void traducirLetras_Click(object sender, EventArgs e)
+        {
+            string text = textBox1.Text, traduccion = null;
+            string[] bytes;
+            char[] numeroBinario;
+            int exponente, cifra, caracterInt;
+            bool centinela = true, esByte = true;
+
+            bytes = text.Split(' ');
+
+            for(int i=0; i < bytes.Length; i++)
+            {
+                numeroBinario = bytes[i].ToCharArray();
+
+                //REVISA SI LA INFORMACIÓN ES EN BYTES
+                if(numeroBinario.Length != 8)
+                {
+                    MessageBox.Show("La información debe ser introducida en bytes.", "ERROR");
+                    centinela = false;
+                    break;
+                }
+
+                exponente = 7;
+                caracterInt = 0;
+
+                //ASCII DEL 0 ES 48 Y DEL 1 ES 49
+                for(int j=0; j < numeroBinario.Length; j++)
+                {
+                    cifra = ((int)numeroBinario[j] - 48) * (int)Math.Pow(2, exponente);
+                    caracterInt += cifra;
+                    exponente--;
+
+                    if (numeroBinario[j] != '1' && numeroBinario[j] != '0')
+                    {
+                        MessageBox.Show("La información debe ser introducida en bytes.", "ERROR");
+                        centinela = false;
+                        esByte = false;
+                        break;
+                    }
+                }
+
+                if(!esByte)
+                {
+                    break;
+                }
+
+                traduccion += string.Join("", (char)caracterInt);
+            }
+
+            if (centinela && esByte)
+            {
+                MessageBox.Show(traduccion, "Texto traducido a caracteres");
+
+                DialogResult resp = MessageBox.Show("Desea copiar el texto al portapapeles?", "Texto traducido a caracteres", MessageBoxButtons.YesNo);
+
+                if (resp == DialogResult.Yes)
+                {
+                    System.Windows.Forms.Clipboard.SetDataObject(traduccion, true);
+                }
+            }
+        }
+
+        //INICIA LA TRADUCCIÓN DE CARACTER A BINARIO
+        private void traducirBinario_Click(object sender, EventArgs e)
+        {
+            string texto = textBox1.Text, traduccion = null;
+            char[] arregloCaracteres = texto.ToCharArray();
+            char car;
+            int i = 0, valorCar;
+
+            while(i < arregloCaracteres.Length)
+            {
+                car = arregloCaracteres[i];
+                valorCar = (int)car;
+
+                if(i == 0)
+                {
+                    traduccion += intEnBinario(valorCar);
+                }
+                else
+                    traduccion += (" " + intEnBinario(valorCar));
+
+                i++;
+            }
+
+            MessageBox.Show(traduccion, "Texto traducido a binario");
+
+            DialogResult resp = MessageBox.Show("Desea copiar el texto al portapapeles?", "Texto traducido a binario", MessageBoxButtons.YesNo);
+
+            if(resp == DialogResult.Yes)
+            {
+                System.Windows.Forms.Clipboard.SetDataObject(traduccion, true);
+            }
+        }
+
+        //CONVERSIÓN DE UN ENTERO A BINARIO EN BYTES
+        private string intEnBinario(int valorCar)
+        {
+            string binario = null;
+            int digito, numBinario=0, exponente = 0;
+
+            while(valorCar > 0)
+            {
+                digito = valorCar % 2;
+                valorCar /= 2;
+                numBinario += (digito * (int)Math.Pow(10, exponente));
+                exponente++;
+            }
+
+            //Usamos el exponente como la cantidad de digitos para poder rellenar el byte
+
+            int cantCeros = 8 - exponente;
+
+            for(int i=0; i < cantCeros; i++)
+            {
+                binario += "0";
+            }
+
+            binario += numBinario.ToString();
+
+            return binario;
+        }
+    }
+}
+
+/*          //CADENA A STRING
+            char[] su;
+
+            su = new char[2];
+
+            su[0] = 'h';
+            su[1] = 'i';
+
+            string hi = string.Join(" ", su);
+
+            //STRING A CADENA DE CARACTERES
+
+            string prueba = "hola";
+            char[] arr;
+
+            arr = prueba.ToCharArray();
+
+            string texto = arr[3].ToString();
+
+            //STRING A CADENA DE STRINGS
+
+            string prueba2 = "hola que tal";
+            string[] array;
+
+            array = prueba2.Split(' ');
+
+            //INT A CARACTER
+
+            int numero = 65;
+
+            string prueba3 = ((char)numero).ToString();
+
+            MessageBox.Show(texto);*/
